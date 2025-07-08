53805dabb922c69886fe491794dbc11d18d00230
DarkDog2020 <851788565@qq.com>
2025-07-08 15:57:30 +0800

    新增了前端相关库

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,7 +18,6 @@ export default defineConfig({
         // target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         // keep `/api` prefix so backend routes like `/api/users` work
-        rewrite: (path) => path
       }
     }
   },
